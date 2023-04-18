764b0641c6984e6dd1ef55a527ce60bf20fc7a09
Mohsin Raza <mohsin.raza@10pearls.com>
2023-04-18 22:40:45 +0500

    03: H2 database setup

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users VALUES (101, 'mohsin@test.com', 'Mohsin', 'Raza', 'admin', 'ssn101', 'mohsin.test')
+INSERT INTO users VALUES (102, 'ali@test.com', 'Ali', 'Khan', 'admin', 'ssn102', 'ali.test')
+INSERT INTO users VALUES (103, 'ahmed@test.com', 'Ahmed', 'Shaikh', 'admin', 'ssn103', 'ahmed.test')
