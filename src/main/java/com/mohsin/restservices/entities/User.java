2b3727706a4d2380a408485ff4a5906747ccfaa9
Syed Mohsin Raza <97949976+mohsinraza-10P@users.noreply.github.com>
2023-04-29 19:31:58 +0500

    03-JPA-UserMgmtService-Base (#2)
    * 03: User entity added
    
    * 03: H2 database setup
    
    * 03: Repository, Service & Controller Setup - GetAllUsers
    
    * 03: User management completed

---
+++ src/main/java/com/mohsin/restservices/entities/User.java
@@ -0,0 +1,112 @@
+package com.mohsin.restservices.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(length = 50, nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "first_name", length = 50, nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", length = 50, nullable = false)
+    private String lastName;
+
+    @Column(name = "email_address", length = 50, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 50, nullable = false)
+    private String role;
+
+    @Column(length = 50, nullable = false, unique = true)
+    private String ssn;
+
+    public User() {
+    }
+
+    public User(Long id, String username, String firstName, String lastName, String email, String role, String ssn) {
+        this.id = id;
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.role = role;
+        this.ssn = ssn;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getSSN() {
+        return ssn;
+    }
+
+    public void setSSN(String ssn) {
+        this.ssn = ssn;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", role='" + role + '\'' +
+                ", ssn='" + ssn + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/mohsin/restservices/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.mohsin.restservices.repositories;
+
+import com.mohsin.restservices.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+}
