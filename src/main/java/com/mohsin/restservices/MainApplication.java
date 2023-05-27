535fa22ef787f18947e0e8e4400027af4e80f2f2
Syed Mohsin Raza <97949976+mohsinraza-10P@users.noreply.github.com>
2023-05-28 01:39:22 +0500

    08-Internationalization (#7)

--- src/main/java/com/mohsin/restservices/MainApplication.java
+++ src/main/java/com/mohsin/restservices/MainApplication.java
@@ -2,11 +2,31 @@ package com.mohsin.restservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class MainApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        AcceptHeaderLocaleResolver resolver = new AcceptHeaderLocaleResolver();
+        resolver.setDefaultLocale(Locale.US);
+        return resolver;
+    }
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setBasename("messages");
+        return source;
     }
 }
--- src/main/java/com/mohsin/restservices/controllers/HelloWorldController.java
+++ src/main/java/com/mohsin/restservices/controllers/HelloWorldController.java
@@ -1,11 +1,20 @@
 package com.mohsin.restservices.controllers;
 
 import com.mohsin.restservices.models.UserInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private ResourceBundleMessageSource messageSource;
 
     @GetMapping("/hello")
     public String sayHello() {
@@ -15,5 +24,15 @@ public class HelloWorldController {
     @GetMapping("/hello-bean")
     public UserInfo getUserInfo() {
         return new UserInfo("Mohsin", "Raza", "Karachi");
+    }
+
+    @GetMapping("/hello-i18n")
+    public String getI18NMessages(@RequestHeader(name = "Accept-Language", required = false) String language) {
+        return messageSource.getMessage("hello_world", null, Locale.of(language));
+    }
+
+    @GetMapping("/hello-i18n-2")
+    public String getI18NMessages() {
+        return messageSource.getMessage("hello_world", null, LocaleContextHolder.getLocale());
     }
 }
