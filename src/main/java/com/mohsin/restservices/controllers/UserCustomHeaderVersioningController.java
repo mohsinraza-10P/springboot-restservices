51f33fc214650269328ff786148434e7c2ab7da7
Mohsin Raza <mohsin.raza@10pearls.com>
2023-06-13 10:17:21 +0500

    11: Custom header versioning

---
+++ src/main/java/com/mohsin/restservices/controllers/UserCustomHeaderVersioningController.java
@@ -0,0 +1,43 @@
+package com.mohsin.restservices.controllers;
+
+import com.mohsin.restservices.dtos.UserDtoV1;
+import com.mohsin.restservices.dtos.UserDtoV2;
+import com.mohsin.restservices.entities.User;
+import com.mohsin.restservices.exceptions.UserNotFoundException;
+import com.mohsin.restservices.services.UserService;
+import jakarta.validation.constraints.Min;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@Validated
+@RequestMapping(value = "/versioning-header/users")
+public class UserCustomHeaderVersioningController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @GetMapping(value = "/{id}", headers = "api-version=1")
+    public UserDtoV1 getUserByIdV1(@PathVariable("id") @Min(1) Long id) throws UserNotFoundException {
+        Optional<User> userOptional = userService.getUserById(id);
+        User user = userOptional.get();
+        return modelMapper.map(user, UserDtoV1.class);
+    }
+
+    @GetMapping(value = "/{id}", headers = "api-version=2")
+    public UserDtoV2 getUserByIdV2(@PathVariable("id") @Min(1) Long id) throws UserNotFoundException {
+        Optional<User> userOptional = userService.getUserById(id);
+        User user = userOptional.get();
+        return modelMapper.map(user, UserDtoV2.class);
+    }
+}
